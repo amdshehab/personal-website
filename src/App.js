436ed266c1eeff5239a12f54fc4672e3d2996a4d
Shehab <shehab.ahmed.uk@gmail.com>
2019-09-10 21:32:45 +0100

    feat: adds background image styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-
+import logo from "./logo.svg";
 import Container from "./components/container";
 import Box from "./components/box";
 
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <Container>
         <div>hello</div>
-        <Box size="small">
-          <div>world</div>
+        <Box size="large" backgroundImage={logo}>
+          <div></div>
         </Box>
       </Container>
     </div>
--- src/components/box/index.js
+++ src/components/box/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
-const box = ({ children, size, className = "" }) => (
+const box = ({ children, size, backgroundImage = "", className = "" }) => (
   <div className={className}>{children}</div>
 );
 
@@ -9,6 +9,13 @@ export default styled(box)`
   margin: 10px;
   width: 100%;
   height: 100%;
+  ${({ backgroundImage }) =>
+    backgroundImage &&
+    css`
+      background-image: url(${backgroundImage});
+      background-repeat: no-repeat;
+      background-position: center;
+    `}
   ${({ size }) =>
     size === "large"
       ? css`
